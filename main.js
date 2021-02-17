f4401d1ac25a751c8f40702597d6ce32a968b69e
Nazmus Nasir <nazmus.nasir@gmail.com>
2021-02-17 01:58:51 -0500

    pre tutorial setup

--- main.js
+++ main.js
@@ -20,19 +20,4 @@ window.onload = function () {
 
     let el = document.getElementById("autocomplete-list");
 
-    // ac.autocomplete(arr, el);
-    // ac.autocomplete(obj, el, "city");
-
-    let process_fetched = function (data){
-        data.map((item) => {
-            item.car = `${item.Year} ${item.Make} ${item.Model}`;
-            return item;
-        });
-        ac.autocomplete(data, el, "car")
-    }
-
-    const resp = fetch('cars.json')
-        .then((response) => response.json())
-        .then((data) => process_fetched(data));
-
 }
